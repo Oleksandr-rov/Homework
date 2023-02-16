0c111c243b188fab08f95b36b732505e9fbcffc4
Oleksandr-rov <aleksandr.rav@gmail.com>
2023-02-16 13:01:19 +0200

    Homework 10

--- src/main/java/module_10/TestMod10.java
+++ src/main/java/module_10/TestMod10.java
@@ -1,5 +1,8 @@
 package module_10;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -7,8 +10,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class TestMod10 {
-    public static void validNumbers (String pathFile) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(pathFile))) {
+    public static void validNumbers() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/text.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 isCorrectNumbers(line);
@@ -17,6 +20,7 @@ public class TestMod10 {
             e.printStackTrace();
         }
     }
+
     public static void isCorrectNumbers(String text) {
         String strReg = "(\\(\\d{3}\\)\\s\\d{3}-\\d{4}\\b|\\d{3}-\\d{3}-\\d{4}\\b)";
         Pattern p = Pattern.compile(strReg);
@@ -24,24 +28,18 @@ public class TestMod10 {
         if (m.find()) System.out.println(text);
     }
 
-    public static void main(String[] args) {
-        String pathFile = "src/main/resources/text.txt";
-        String pathFile2 = "src/main/resources/word.txt";
-
-        validNumbers(pathFile);
-
+    public static void RepeatedWords() {
         Map<String, Integer> map = new HashMap<String, Integer>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(pathFile2))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/word.txt"))) {
             String str;
             Integer n = 0;
             while ((str = reader.readLine()) != null) {
                 String[] line = str.split("\\s+");
-                for (String s: line) {
+                for (String s : line) {
                     if (map.isEmpty()) map.put(s, ++n);
                     else if ((n = map.get(s)) != null) {
                         map.put(s, ++n);
-                    }
-                    else map.put(s, 1);
+                    } else map.put(s, 1);
                 }
             }
         } catch (IOException e) {
@@ -50,8 +48,40 @@ public class TestMod10 {
 
         Set<Map.Entry<String, Integer>> set = map.entrySet();
         List<Map.Entry<String, Integer>> sorted = set.stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).collect(Collectors.toList());
-        for (Map.Entry<String, Integer> gg: sorted)
+        for (Map.Entry<String, Integer> gg : sorted)
             System.out.println(gg.getKey() + " " + gg.getValue());
+    }
 
+    public static void ObjectsToJson() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<User> users = new ArrayList<>();
+
+        try (InputStream fis = new FileInputStream("src/main/resources/file.txt");
+             Scanner scanner = new Scanner(fis)) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] splitedLine = line.split(" ");
+                if (splitedLine[1].matches("[-+]?\\d+")) {
+                    users.add(new User(splitedLine[0], Integer.parseInt(splitedLine[1])));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter fileWriter = new FileWriter("src/main/resources/users.json")) {
+            gson.toJson(users, fileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Task #1");
+        validNumbers();
+        System.out.println("\nTask #2");
+        RepeatedWords();
+        System.out.println("\nTask #3");
+        RepeatedWords();
     }
 }
